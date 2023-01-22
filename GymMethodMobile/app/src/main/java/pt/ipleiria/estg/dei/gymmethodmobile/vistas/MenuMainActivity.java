d4e7ab2b5f90d5a7cac9a6f61b82e31c26b46f3c
Figueiras <antoniorfigueiras@gmail.com>
2023-01-22 11:33:17 +0000

    layouts

--- GymMethodMobile/app/src/main/java/pt/ipleiria/estg/dei/gymmethodmobile/vistas/MenuMainActivity.java
+++ GymMethodMobile/app/src/main/java/pt/ipleiria/estg/dei/gymmethodmobile/vistas/MenuMainActivity.java
@@ -27,94 +27,91 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MenuMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-        private NavigationView navigationView;
-        private DrawerLayout drawer;
-        private Integer user_id;
-        private String token;
-        private String username;
-        private FragmentManager fragmentManager;
-        public static final String SHARED_USER="DADOS_USER"; // CHAVE
-        public static final String USER_ID="ID"; // ID
-        public static final String USERNAME="USER"; // NOME
-        public static final String OPERACAO="OPERACAO";
-        public static final String PASSWORD="PASSWORD";
-        public static final String TOKEN="TOKEN";
-        public static final int ADD=10, EDIT=20, DELETE=30;
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private Integer user_id;
+    private String token;
+    private String username;
+    private FragmentManager fragmentManager;
+    public static final String SHARED_USER = "DADOS_USER"; // CHAVE
+    public static final String USER_ID = "ID"; // ID
+    public static final String USERNAME = "USER"; // NOME
+    public static final String OPERACAO = "OPERACAO";
+    public static final String PASSWORD = "PASSWORD";
+    public static final String TOKEN = "TOKEN";
+    public static final int ADD = 10, EDIT = 20, DELETE = 30;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_menu_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_main);
 
 
-            Toolbar toolbar = findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
-            drawer = findViewById(R.id.drawerLayout);
-            navigationView = findViewById(R.id.navView);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navView);
 
-            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
-                    toolbar, R.string.ndOpen, R.string.ndClose);
-            toggle.syncState();
-            drawer.addDrawerListener(toggle);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
+                toolbar, R.string.ndOpen, R.string.ndClose);
+        toggle.syncState();
+        drawer.addDrawerListener(toggle);
 
-            carregarCabecalho();
+        carregarCabecalho();
 
-            navigationView.setNavigationItemSelectedListener(this);
-            fragmentManager = getSupportFragmentManager();
-            carregarFragmentoInicial();
+        navigationView.setNavigationItemSelectedListener(this);
+        fragmentManager = getSupportFragmentManager();
+        carregarFragmentoInicial();
+    }
+
+    private boolean carregarFragmentoInicial() {
+        Menu menu = navigationView.getMenu();
+        MenuItem item = menu.getItem(0);
+        item.setChecked(true);
+        return onNavigationItemSelected(item);
+    }
+
+    private void carregarCabecalho() {
+        user_id = getIntent().getIntExtra(USER_ID, 0);
+        username = getIntent().getStringExtra(USERNAME);
+        token = getIntent().getStringExtra(TOKEN);
+        SharedPreferences infoUser = getSharedPreferences(SHARED_USER, Context.MODE_PRIVATE);
+
+        if (username != null && token != null) {
+            SharedPreferences.Editor editor = infoUser.edit();
+            editor.putInt(USER_ID, user_id);
+            editor.putString(USERNAME, username);
+            editor.putString(TOKEN, token);
+            editor.apply();
+        } else
+            username = infoUser.getString(USERNAME, getString(R.string.default_email));
+
+        if (username != null) {
+            View headerView = navigationView.getHeaderView(0);
+            TextView tvEmail = headerView.findViewById(R.id.tvMainEmail); // Para ir buscar ao cabeçalho do navigation view
+            tvEmail.setText(username);
         }
 
-        private boolean carregarFragmentoInicial() {
-            Menu menu = navigationView.getMenu();
-            MenuItem item = menu.getItem(0);
-            item.setChecked(true);
-            return onNavigationItemSelected(item);
-        }
+    }
 
-        private void carregarCabecalho() {
-            user_id = getIntent().getIntExtra(USER_ID, 0);
-            username = getIntent().getStringExtra(USERNAME);
-            token= getIntent().getStringExtra(TOKEN);
-            SharedPreferences infoUser = getSharedPreferences(SHARED_USER, Context.MODE_PRIVATE);
-
-            if(username!=null && token!=null)  {
-                SharedPreferences.Editor editor =infoUser.edit();
-                editor.putInt(USER_ID, user_id);
-                editor.putString(USERNAME, username);
-                editor.putString(TOKEN, token);
-                editor.apply();
-            }
-
-            else
-                username=infoUser.getString(USERNAME, getString(R.string.default_email));
-
-            if (username != null){
-                View headerView = navigationView.getHeaderView(0);
-                TextView tvEmail = headerView.findViewById(R.id.tvMainEmail); // Para ir buscar ao cabeçalho do navigation view
-                tvEmail.setText(username);
-            }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.planos:
+                fragment = new ListaPlanosFragment();
+                setTitle(item.getTitle());
+                break;
+            case R.id.perfil:
+                fragment = new PerfilFragment();
+                setTitle(item.getTitle());
 
         }
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment fragment = null;
-            switch (item .getItemId()) {
-                case R.id.planos:
-
-                    fragment = new ListaPlanosFragment();
-                    setTitle(item.getTitle());
-                    break;
-                case R.id.perfil:
-                    fragment = new PerfilFragment();
-                    setTitle(item.getTitle());
-
-            }
-            if (fragment != null)
-                fragmentManager.beginTransaction().replace(R.id.contentFragment, fragment).commit();
-            drawer.closeDrawer(GravityCompat.START);
-            return true;
-        }
+        if (fragment != null)
+            fragmentManager.beginTransaction().replace(R.id.contentFragment, fragment).commit();
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
 
 }
